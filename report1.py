d8586d9757012983143b55090c77f0e070e0f365
KwonBK0223 <114387888+KwonBK0223@users.noreply.github.com>
2023-03-22 22:13:26 +0900

    Add files via upload

---
+++ report1.py
@@ -0,0 +1,57 @@
+
+# 3번
+# print("Hello Python!")
+# print("100"+"200")
+# print(100+200)
+
+# 4번
+# hour = 2
+# length = 190
+# v = 190/2
+# print(v)
+
+# 5번
+# length_Earth_Sun = 149597870
+# length_Light_second = 299792
+# ans_sec = length_Earth_Sun/length_Light_second
+# ans_min = ans_sec/60
+# print(ans_sec,"sec")
+# print(ans_min,"min")
+
+# #6번
+# width = 30
+# height = 60
+# area = width*height
+# print(area)
+
+# 7번
+# width = 40
+# height = 20
+# area = (width*height)/2
+# print(area)
+
+# 8번
+# radius = 11
+# PI = 3.141592
+# length_circle = 2*radius*PI 
+# area_circle = (radius**2)*PI
+# print(length_circle)
+# print(area_circle)"""
+
+# 9번
+# a = int(input("세 자리 정수를 입력하세오 :"))
+# ans1 = a//100
+# ans2 = (a-(ans1*100))//10
+# ans3 = a%10
+# print('백의 자리 :',ans1)
+# print('십의 자리 :',ans2)
+# print('일의 자리 :',ans3)
+
+# 10번
+# a = int(input("세 자리 정수를 입력하세오 :"))
+# ans1 = a//100
+# ans2 = (a-(ans1*100))//10
+# ans3 = a%10
+# print(ans3)
+# print(ans2)
+# print(ans1)
