35c8b13091cd8a479133a396088ac805a5038d99
KwonBK0223 <114387888+KwonBK0223@users.noreply.github.com>
2023-03-22 22:14:26 +0900

    Add files via upload

---
+++ 컴퓨터및프로그래밍입문/02_var.py
@@ -0,0 +1,10 @@
+# radius = 4.0
+# radius = 7.0
+
+# 변수 선언과 값 할당
+# print('원의 반지름', radius)
+# print('원의 면적',3.14* radius * radius)
+# print('원의 둘레',2.0*3.14* radius)
+
+# 바인딩
+
---
+++ 컴퓨터및프로그래밍입문/03_control.py
@@ -0,0 +1,45 @@
+# age = 18
+# if age < 20:
+#     print("청소년 할인")
+#     print("감사합니다")
+# else:
+#     print("다시 들러주세요!")
+
+# 3의 배수 판단
+# number = int(input("정수를 입력해주세요! : "))
+# if (number % 3 == 0) and (number % 5 == 0):
+#     print("3또는 5의 배수입니다.")
+# else:
+#     print("3과 5의 배수가 아닙니다.")
+
+# A,B,...
+# score = int(input("점수는 ? : "))
+# if score >= 90:
+#     grade = "A"
+# elif score >= 80:
+#     grade = "B"
+# elif score >= 70:
+#     grade = "C"
+# elif score >= 60:
+#     grade = "D"
+# else :
+#     grade = "F"
+# print("학점은 : ",grade)
+
+# print(range(3))
+
+# for i in range(8):
+#     print(i)
+
+
+# for i in "Apple":
+#     print(i)
+
+# for _ in range(5):
+#     print("A")
+
+# s = 0
+# for i in range(1,11):
+#     s = s + i
+# print(s)
+
