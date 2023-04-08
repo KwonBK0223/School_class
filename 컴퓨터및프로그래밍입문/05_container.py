4dd01619cac2fb84ec95c2d39d7ce9d488416f94
KwonBK0223 <114387888+KwonBK0223@users.noreply.github.com>
2023-04-08 22:31:00 +0900

    Add files via upload

---
+++ 컴퓨터및프로그래밍입문/05_container.py
@@ -0,0 +1,47 @@
+# lst=[1,2,3,4,5,6]
+# print(lst)
+
+# sum = 0
+# for i in range(5):
+#     sum = sum + i
+# print(sum)
+
+# lst =[]
+# for i in range(5):
+#     lst.append(i)
+# print(lst)
+
+# lst =[]
+# tup = (1,2,3,4,5,6,7,8,9,10)
+# for i in tup:
+#     lst.append(i)
+# print(lst)
+
+# lst =[]
+# s={1,2,3,4,5,6,7,8,9,10}
+# for i in s:
+#     lst.append(i)
+# print(lst)
+
+# lst =[]
+# for i in list(range(5)):
+#     lst.append(i)
+# print(lst)
+# print(list(range(5)))
+# print(range(5))
+
+# lst =[]
+# for i in list(range(100,1000,10)):
+#     lst.append(i)  #뒤에 추가
+# print(lst)
+
+# lst=[]
+# for i in list(range(5)):
+#     lst.append(i)
+# print(lst)
+
+# lst =[10,11,12,13,14,15]
+# print(lst[0:3])  #슬라이싱 #0부터세는거 주의!!!
+# print(lst[-1])   #뒤에는 -1부터 -2, -3 ...
+# lst.remove(10)   #삭제 => 값이 있는지 없는지 먼저 체크해야됨
+# print(lst)
